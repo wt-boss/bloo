cfa2bb3bbe0a7bd48db44e79bb4c02e20fcac9d7
FrankyNOX <53083176+FrankyNOX@users.noreply.github.com>
2020-05-29 19:22:28 +0200

    Creation du sondage gratuit

---
+++ resources/views/questionnaire/validate.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">Validation des informations du formulaire</div>
+                    <div class="card-body">
+                        <form method="post" action="{{route('questionnaire.validate_free',[$questionnaire->id])}}">
+                            @csrf
+                            <div class="row">
+                                <div class="form-group col-6">
+                                    <label for="titre">Titre</label>
+                                    <input type="text" class="form-control" id="titre" aria-describedby="titre" value="{{$questionnaire->title}}" disabled>
+                                </div>
+                                <div class="form-group col-6">
+                                    <label for="objectif">Objectif</label>
+                                    <input type="text" class="form-control" id="objectif"  value="{{$questionnaire->purpose}}" disabled>
+                                </div>
+                                <div class="form-group col-8" >
+                                    <label for="exampleInputEmail1">Token</label>
+                                    <input type="text" class="form-control" id="token" aria-describedby="token" value="{{$questionnaire->token}}" disabled>
+                                    <small id="token" class="form-text text-muted">Vous devez garder ce token pour avoir acces a votre formulaire plus tard</small>
+                                </div>
+                                <div class="form-group col-4">
+                                    <label for="exampleInputEmail1">Mot de passe</label>
+                                    <input type="password" class="form-control" name="password" id="password" aria-describedby="password">
+                                    <small id="password"  class="form-text text-muted">Entrer un mot de passe pour avoir acces a votre formulaire prochainement</small>
+                                </div>
+                            </div>
+
+                            <button type="submit" class="btn btn-primary">Valider</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
