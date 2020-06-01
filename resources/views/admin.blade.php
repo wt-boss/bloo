ef5844a6ea6044de2d8700dc15aa74ec17ee77cb
FrankyNOX <53083176+FrankyNOX@users.noreply.github.com>
2020-06-01 14:00:00 +0200

    Test de recuperation

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,6 +1,29 @@
-
 @extends('adminlte::page')
 
-
+@section('content')
+        <div class="container-fluid">
+            <!-- Small boxes -->
+           <div class="row">
+               <div class="col-6 col-lg-3">
+                  <!-- small box -->
+                   <div class="small-box bg-info">
+                       <div class="inner">
+                           <h3>150</h3>
+                           <p>New Order</p>
+                       </div>
+                       <div class="icon">
+                           <i class="ion ion-bag">
+                           </i>
+                           <a href="#" class="small-box-footer">
+                               "More info"
+                               <i class="fas fa-arrow-circle-lig">
+                               </i>
+                           </a>
+                       </div>
+                   </div>
+               </div>
+           </div>
+       </div>
+@endsection
 
 
