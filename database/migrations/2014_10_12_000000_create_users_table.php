a430db9a3c887df2b929cd5ee4192a3e9d0aadc2
FrankyNOX <53083176+FrankyNOX@users.noreply.github.com>
2020-06-22 09:30:39 +0200

    Update migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,15 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('role')->default(0);
+            $table->integer('active')->default(0);
+            $table->string('avatar')->nullable();
+            $table->softDeletes();
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2020_06_11_113539_create_entreprises_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEntreprisesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('entreprises', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('nom');
+            $table->string('addrese');
+            $table->string('Numero_contribuable')->nullable();
+            $table->string('numero_siret')->nullable();
+            $table->string('pays');
+            $table->string('ville');
+            $table->string('telephone');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('entreprises');
+    }
+}
