5a4e76b10414bf194707af8d3c2f270cf30de476
FrankyNOX <53083176+FrankyNOX@users.noreply.github.com>
2020-07-08 21:52:53 +0200

    fix bugs

--- bootstrap/cache/packages.php
+++ bootstrap/cache/packages.php
@@ -10,6 +10,17 @@
       'Currency' => 'AkibTanjim\\Currency\\Facades\\CurrencyConverter',
     ),
   ),
+  'danielme85/laravel-cconverter' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'danielme85\\CConverter\\CConverterServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Currency' => 'danielme85\\CConverter\\CConverter',
+    ),
+  ),
   'devmarketer/easynav' => 
   array (
     'providers' => 
@@ -37,6 +48,17 @@
     'providers' => 
     array (
       0 => 'Fideloper\\Proxy\\TrustedProxyServiceProvider',
+    ),
+  ),
+  'florianv/laravel-swap' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Swap\\Laravel\\SwapServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Swap' => 'Swap\\Laravel\\Facades\\Swap',
     ),
   ),
   'fruitcake/laravel-cors' => 
@@ -75,6 +97,13 @@
       0 => 'Laravel\\Tinker\\TinkerServiceProvider',
     ),
   ),
+  'laravel/ui' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Ui\\UiServiceProvider',
+    ),
+  ),
   'laravelcollective/html' => 
   array (
     'providers' => 
@@ -85,28 +114,6 @@
     array (
       'Form' => 'Collective\\Html\\FormFacade',
       'Html' => 'Collective\\Html\\HtmlFacade',
-    ),
-  ),
-  'maatwebsite/excel' => 
-  array (
-    'providers' => 
-    array (
-      0 => 'Maatwebsite\\Excel\\ExcelServiceProvider',
-    ),
-    'aliases' => 
-    array (
-      'Excel' => 'Maatwebsite\\Excel\\Facades\\Excel',
-    ),
-  ),
-  'mews/purifier' => 
-  array (
-    'providers' => 
-    array (
-      0 => 'Mews\\Purifier\\PurifierServiceProvider',
-    ),
-    'aliases' => 
-    array (
-      'Purifier' => 'Mews\\Purifier\\Facades\\Purifier',
     ),
   ),
   'nesbot/carbon' => 
@@ -121,6 +128,13 @@
     'providers' => 
     array (
       0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
+    ),
+  ),
+  'riverskies/laravel-newsletter-subscription' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Riverskies\\LaravelNewsletterSubscription\\Providers\\NewsletterSubscriptionServiceProvider',
     ),
   ),
 );
