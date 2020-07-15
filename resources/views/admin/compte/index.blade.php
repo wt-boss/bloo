3a27a91e44118bc7c84b0a04a385defd91cb495e
Cedric NOUMBO <noumbopro@hotmail.com>
2020-07-16 00:42:57 +0100

    main design dasuboard layout okay

--- resources/views/admin/compte/index.blade.php
+++ resources/views/admin/compte/index.blade.php
@@ -18,77 +18,6 @@
 
 @section('content')
 
-    <div class="row">
-        @foreach($comptes as $compte)
-        <div class="col-md-3">
-            <!-- Widget: user widget style 1 -->
-            <div class="box box-widget widget-user">
-                <!-- Add the bg color to the header using any of the bg-* classes -->
-                <div class="widget-user-header bg-aqua-active">
-                    <h3 class="widget-user-username">Alexander Pierce</h3>
-                    <h5 class="widget-user-desc">Founder &amp; CEO</h5>
-                </div>
-                <div class="widget-user-image">
-                    <img class="img-circle" src="../dist/img/user1-128x128.jpg" alt="User Avatar">
-                </div>
-                <div class="box-footer">
-                    <div class="row">
-                        <div class="col-sm-4 border-right">
-                            <div class="description-block">
-                                <h5 class="description-header">3,200</h5>
-                                <span class="description-text">SALES</span>
-                            </div>
-                            <!-- /.description-block -->
-                        </div>
-                        <!-- /.col -->
-                        <div class="col-sm-4 border-right">
-                            <div class="description-block">
-                                <h5 class="description-header">13,000</h5>
-                                <span class="description-text">FOLLOWERS</span>
-                            </div>
-                            <!-- /.description-block -->
-                        </div>
-                        <!-- /.col -->
-                        <div class="col-sm-4">
-                            <div class="description-block">
-                                <h5 class="description-header">35</h5>
-                                <span class="description-text">PRODUCTS</span>
-                            </div>
-                            <!-- /.description-block -->
-                        </div>
-                        <!-- /.col -->
-                    </div>
-                    <!-- /.row -->
-                </div>
-            </div>
-            <!-- /.widget-user -->
-        </div>
-            @endforeach
-            <div class="col-md-3">
-                <!-- Widget: user widget style 1 -->
-                <div class="box box-widget widget-user">
-                    <!-- Add the bg color to the header using any of the bg-* classes -->
-                    <div class="widget-user-header bg-aqua-active">
-                        <h3 class="widget-user-username">Alexander Pierce</h3>
-                        <h5 class="widget-user-desc">Founder &amp; CEO</h5>
-                    </div>
-                    <div class="widget-user-image">
-                        <img class="img-circle" src="../dist/img/user1-128x128.jpg" alt="User Avatar">
-                    </div>
-                    <div class="box-footer">
-                        <div class="row">
-                            <div class=" col-8">
-                                <div class="description-block">
-                                    <h5 class="description-header">35</h5>
-                                    <span class="description-text">PRODUCTS</span>
-                                </div>
-                        </div>
-                        <!-- /.row -->
-                    </div>
-                </div>
-                <!-- /.widget-user -->
-            </div>
-    </div>
 @endsection
 
 @section('laraform_script1')
@@ -96,5 +25,11 @@
     <script src="{{ asset('assets/js/core/libraries/jquery.min.js') }}"></script>
     <script src="{{ asset('assets/js/core/libraries/bootstrap.min.js') }}"></script>
     <script src="{{ asset('assets/js/plugins/blockui.min.js') }}"></script>
+@endsection
 
+
+@section('laraform_script2')
+    {{-- <script src="{{ asset('assets/js/core/app.js') }}"></script> --}}
+    <script src="{{ asset('assets/js/plugins/ripple.min.js') }}"></script>
+    <script src="{{ asset('assets/js/custom/main.js') }}"></script>
 @endsection
--- resources/views/admin/operation/index.blade.php
+++ resources/views/admin/operation/index.blade.php
@@ -2,18 +2,6 @@
 
 @extends('admin.top-nav')
 
-@section('laraform_style')
-    <!-- Laraform Link Style -->
-    <link href="{{ asset('favicon.ico') }}" rel="icon">
-    <link href="https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900" rel="stylesheet">
-    <link href="{{ asset('assets/css/icons/icomoon/styles.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/css/core.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/css/components.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/css/colors.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/css/custom.css') }}" rel="stylesheet">
-@endsection
-
 @section('content')
     <div class="panel panel-flat border-left-xlg border-left-primary">
         <div class="panel-heading">
