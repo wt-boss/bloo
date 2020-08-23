f1ffa702c7ffa27f9500d0faef6392e0991bb420
Michael <michaelnsom2@gmail.com>
2020-08-23 19:14:07 +0200

    update footer

--- resources/views/layouts/frontend/partial/footer.blade.php
+++ resources/views/layouts/frontend/partial/footer.blade.php
@@ -37,16 +37,16 @@
           <div class="ftco-footer-widget mb-4">
               <div class="block-23 mb-3">
                 <ul>
-                  <li><span class="icon icon-map-marker"></span><span class="text">Deido, Douala</span></li>
+                  <li><span class="icon icon-map-marker"></span><span class="text">Bonamoussadi, Douala - Cameroun</span></li>
 
-                  <li><a href="#"><span class="icon icon-envelope"></span><span class="text">bloo@bloosite.com</span></a></li>
+                  <li><a href="#"><span class="icon icon-envelope"></span><span class="text">hello@blooapp.live</span></a></li>
 
                 </ul>
               </div>
             <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
               <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
-              <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
-              <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+              <li class="ftco-animate"><a href="#"><span class="icon-linkedin"></span></a></li>
+              <li class="ftco-animate"><a href="#"><span class="icon-youtube"></span></a></li>
             </ul>
           </div>
         </div>
