68142c4a0ecf3e5b2d63cb66dfbc35839cab3c4f
Cedric NOUMBO <noumbopro@hotmail.com>
2020-09-24 23:08:44 +0100

    remove test tag - localization

--- app/Http/Middleware/Locale.php
+++ app/Http/Middleware/Locale.php
@@ -20,7 +20,6 @@ class Locale
         if(!session()->has('locale'))
         {
             $value = $request->cookie('lang_blooapp');
-            dd($value);
             $lang = array('fr', 'en');
             if(in_array($value, $lang)) {
                 session()->put('locale', $value);
