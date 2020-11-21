951b17e0728e85d800de6dd413bec5825508e491
Francois NOX <noah@cnoumbo.com>
2020-11-21 17:36:50 +0100

    fix tables locations bugs again

---
+++ database/migrations/2020_11_21_163525_create_table_new_location.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableNewLocation extends Migration
+{
+        /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('locations', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger("user_id");
+            $table->unsignedBigInteger("operation_id");
+            $table->unsignedBigInteger("site_id")->nullable();
+            $table->float('lat', 60, 25)->nullable();
+            $table->float('lng', 60, 25)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('locations');
+    }
+}
