bf6efc518816c7329ad7e040cd8b0cbfe11bf923
Cedric NOUMBO <noumbopro@hotmail.com>
2020-11-29 19:47:32 +0100

    faire sortir la methode getcity du middleware api:auth

--- app/Http/Controllers/API/UsersController.php
+++ app/Http/Controllers/API/UsersController.php
@@ -15,7 +15,7 @@ class UsersController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth:api', ['except' => 'pieces']);
+        $this->middleware('auth:api', ['except' => 'pieces', 'getcity']);
         $this->guard = "api";
     }
 
