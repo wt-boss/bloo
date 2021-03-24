e901338646174ef417f9662d280389692fbac480
Francois NOX <noah@cnoumbo.com>
2021-03-24 13:58:23 +0100

    Offline online update

--- app/Http/Middleware/LastUserActivity.php
+++ app/Http/Middleware/LastUserActivity.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Middleware;
 
+use Carbon\Carbon;
 use Closure;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Redis;
 
 class LastUserActivity
@@ -19,7 +21,8 @@ class LastUserActivity
     {
         if(Auth::check())
         {
-             Redis::set('online-user-'.Auth::id(), Auth::id(), 'EX', 20);
+            $expireAt = Carbon::now()->addMinutes(1);
+            Cache::put('user-is-online-'.Auth::id() , true , $expireAt);
         }
         return $next($request);
     }
