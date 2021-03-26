2c45ef6d8ba908d9ec29fae8bfedc2a818c87e9c
Francois NOX <noah@cnoumbo.com>
2021-03-26 14:53:34 +0100

    Ajout du cache a toutes les routes connectés

--- app/Http/Controllers/ApiV1/UserController.php
+++ app/Http/Controllers/ApiV1/UserController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers\ApiV1;
 
+use Carbon\Carbon;
 use Exception;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cache;
 use Tymon\JWTAuth\Facades\JWTAuth;
 use App\Http\Controllers\Controller;
 use App\Repositories\Api\ApiRepository;
@@ -13,12 +16,14 @@ class UserController extends Controller
 {
     /**
      * Display user's details
-     * 
+     *
      * @return Illuminate\Http\JsonResponse
      */
     public function me(ApiRepository $apiRepository) {
         try {
             $collect = collect();
+            $expireAt = Carbon::now()->addMinutes(2);
+            Cache::put('user-is-online-'.Auth::id() , true , $expireAt);
             return $apiRepository->jsonResponse(null, Response::HTTP_OK, $collect->push(JWTAuth::user()));
         } catch (Exception $e) {
             return $apiRepository->jsonResponse($e->getMessage());
@@ -39,6 +44,8 @@ class UserController extends Controller
             $user = JWTAuth::user();
             $data = $request->validated();
             $user->update($data);
+            $expireAt = Carbon::now()->addMinutes(2);
+            Cache::put('user-is-online-'.Auth::id() , true , $expireAt);
             return $apiRepository->jsonResponse(trans('update_success'), Response::HTTP_OK, $user);
         }catch(Exception $e){
             return $apiRepository->jsonResponse($e->getMessage());
